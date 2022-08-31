de7308723e43995b936eb4075b1cc13d261e1209
Dylan Lobo <dylan.j.lobo@gmail.com>
2022-08-31 22:41:36 +0100

    Added help message via -h command line argument.

--- cur_track_info.py
+++ cur_track_info.py
@@ -1,8 +1,8 @@
 import dbus_mpris.core as mpris_core
-from typing import Any, List, Dict, Tuple
-from consolemenu import SelectionMenu
-import logging
 import dbus_mpris.helpers as helpers
+from typing import Dict, Any
+import argparse
+import logging
 
 logger = logging.getLogger(__name__)
 
@@ -23,8 +23,21 @@ def print_track_info(
         logger.error(e)
 
 
+def get_cmd_line_args():
+    # Set up command line argument processing
+    parser = argparse.ArgumentParser(
+        description=(
+            "Prints simplifed information about the current track of the selected player instance."
+            "If multiple player instances are running, the user is presented with a selection menu."
+        )
+    )
+    arguments = parser.parse_args()
+    return arguments
+
+
 if __name__ == "__main__":
     try:
+        args = get_cmd_line_args()
         player_instance_names = mpris_core.get_running_player_instance_names()
         if len(player_instance_names) == 0:
             logger.error("No mpris enabled players are running")
