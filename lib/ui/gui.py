95099d32bf7db817f02886e9fd829f221d336fb2
Dylan Lobo <dylan.j.lobo@gmail.com>
2022-12-08 16:12:10 +0000

    Changed dictionary looping to use enumerate

--- lib/ui/gui.py
+++ lib/ui/gui.py
@@ -12,21 +12,26 @@ logger = logging.getLogger(__name__)
 
 
 class ChaptersPanel(ttk.LabelFrame):
-
-    def __init__(self, master: tk.Tk, chapters: List[str],
-                 chapters_selection_action_functs: List[callable]):
+    def __init__(
+        self,
+        master: tk.Tk,
+        chapters: List[str],
+        chapters_selection_action_functs: List[callable],
+    ):
         super().__init__(master, text="Chapters")
         self._chapters = chapters
         self._chapter_selection_action_functs = chapters_selection_action_functs
         lb_height = len(chapters) if len(chapters) < 10 else 10
-        lb = tk.Listbox(self, listvariable=tk.StringVar(value=chapters), width=60, height=lb_height)
+        lb = tk.Listbox(
+            self, listvariable=tk.StringVar(value=chapters), width=60, height=lb_height
+        )
         lb.grid(column=0, row=0, sticky="NWES")
         sv = ttk.Scrollbar(self, orient=tk.VERTICAL, command=lb.yview)
         sv.grid(column=1, row=0, sticky="NS")
-        lb['yscrollcommand'] = sv.set
+        lb["yscrollcommand"] = sv.set
         sh = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=lb.xview)
         sh.grid(column=0, row=1, sticky="EW")
-        lb['xscrollcommand'] = sh.set
+        lb["xscrollcommand"] = sh.set
         lb.bind("<<ListboxSelect>>", self.lb_selection_handler)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -40,14 +45,23 @@ class ChaptersPanel(ttk.LabelFrame):
 
 
 class PlayerControlPanel(ttk.LabelFrame):
-
     def __init__(self, master: tk.Tk, player_controls_funcs: Dict[str, callable]):
         super().__init__(master, text="Player Controls")
-        self.pause_play_button = ttk.Button(self, text="Play/Pause", command=player_controls_funcs["Play/Pause"])
-        self.forward_30sec_button = ttk.Button(self, text=">", command=player_controls_funcs[">"])
-        self.forward_1min_button = ttk.Button(self, text=">>", command=player_controls_funcs[">>"])
-        self.backward_30sec_button = ttk.Button(self, text="<", command=player_controls_funcs["<"])
-        self.backward_1min_button = ttk.Button(self, text="<<", command=player_controls_funcs["<<"])
+        self.pause_play_button = ttk.Button(
+            self, text="Play/Pause", command=player_controls_funcs["Play/Pause"]
+        )
+        self.forward_30sec_button = ttk.Button(
+            self, text=">", command=player_controls_funcs[">"]
+        )
+        self.forward_1min_button = ttk.Button(
+            self, text=">>", command=player_controls_funcs[">>"]
+        )
+        self.backward_30sec_button = ttk.Button(
+            self, text="<", command=player_controls_funcs["<"]
+        )
+        self.backward_1min_button = ttk.Button(
+            self, text="<<", command=player_controls_funcs["<<"]
+        )
         self.backward_1min_button.grid(row=0, column=1, padx=10, pady=10)
         self.backward_30sec_button.grid(row=0, column=2, padx=10)
         self.pause_play_button.grid(row=0, column=3, padx=10)
@@ -79,7 +93,6 @@ class ChaptersGui(tk.Tk):
 
 
 class ChaptersGuiBuilder:
-
     def __init__(self, chapters_filename: str, player: Player):
         self._player_control_panel: PlayerControlPanel = None
         self._chapters_panel: ChaptersPanel = None
@@ -99,26 +112,41 @@ class ChaptersGuiBuilder:
 
     def build_chapters_panel(self):
         listbox_items: List[str] = []
-        index = 1
         chapters_position_functions: List[callable] = []
         chapter: str
         position: str
-        for chapter, position in self._chapters.items():
-            listbox_items.append(f"{index}.    {chapter} ({position})")
-            chapters_position_functions.append(partial(set_player_position, self._player, position))
-            index = index + 1
-        self._chapters_panel = ChaptersPanel(self._main_window, chapters=listbox_items,
-                                             chapters_selection_action_functs=chapters_position_functions)
+        for index, (chapter, position) in enumerate(self._chapters.items()):
+            listbox_items.append(f"{index+1}.    {chapter} ({position})")
+            chapters_position_functions.append(
+                partial(set_player_position, self._player, position)
+            )
+        self._chapters_panel = ChaptersPanel(
+            self._main_window,
+            chapters=listbox_items,
+            chapters_selection_action_functs=chapters_position_functions,
+        )
 
     def build_player_control_panel(self):
         button_action_funcs = {
             "Play/Pause": partial(play_pause_player, self._player),
             ">": partial(skip_player, player=self._player, offset="00:00:10"),
             ">>": partial(skip_player, player=self._player, offset="00:01:00"),
-            "<": partial(skip_player, player=self._player, offset="00:00:10", direction=Direction.REVERSE),
-            "<<": partial(skip_player, player=self._player, offset="00:01:00", direction=Direction.REVERSE)
+            "<": partial(
+                skip_player,
+                player=self._player,
+                offset="00:00:10",
+                direction=Direction.REVERSE,
+            ),
+            "<<": partial(
+                skip_player,
+                player=self._player,
+                offset="00:01:00",
+                direction=Direction.REVERSE,
+            ),
         }
-        self._player_control_panel = PlayerControlPanel(self._main_window, button_action_funcs)
+        self._player_control_panel = PlayerControlPanel(
+            self._main_window, button_action_funcs
+        )
 
 
 def build_gui_menu(chapters_filename: str, player: Player) -> ChaptersGui:
