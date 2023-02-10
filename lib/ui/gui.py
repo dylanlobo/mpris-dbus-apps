70b5ddd1f28b16bd6f15cc60d1d625c6bda7a244
Dylan Lobo <dylan.j.lobo@gmail.com>
2023-02-10 14:03:17 +0000

    Added a Connections menu to the GUI

--- lib/ui/gui.py
+++ lib/ui/gui.py
@@ -5,7 +5,7 @@ from typing import List, Dict
 from functools import partial
 from .. import helpers as mpris_helpers
 from ..helpers import Direction
-from lib.dbus_mpris.core import Player
+from lib.dbus_mpris.core import PlayerProxy, PlayerFactory
 
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
@@ -70,16 +70,18 @@ class PlayerControlPanel(ttk.LabelFrame):
         self.grid(padx=10, pady=10)
 
 
-def set_player_position(player: Player, position: str):
+def set_player_position(player: PlayerProxy, position: str):
     player.set_position(mpris_helpers.to_microsecs(position))
 
 
-def skip_player(player: Player, offset: str, direction: Direction = Direction.FORWARD):
+def skip_player(
+    player: PlayerProxy, offset: str, direction: Direction = Direction.FORWARD
+):
     offset_with_dir = mpris_helpers.to_microsecs(offset) * direction
     player.seek(offset_with_dir)
 
 
-def play_pause_player(player: Player):
+def play_pause_player(player: PlayerProxy):
     player.play_pause()
 
 
@@ -93,9 +95,108 @@ class ChaptersGui(tk.Tk):
         self.resizable(width=False, height=False)
         self.mainloop()
 
+    @property
+    def root_window(self):
+        return self._root
+
+
+class PlayerConnectionPopup(tk.Toplevel):
+    def __init__(self, master: tk.Tk, running_players: Dict):
+        super().__init__(master)
+        self.title("Connect to Player")
+        self._running_players = running_players
+        if not self._running_players:
+            self._create_error_message_panel()
+        else:
+            self._create_players_selection_panel(list(self._running_players.keys()))
+        self.resizable(width=False, height=False)
+        self.grid()
+        self.transient(master)  # set to be on top of the main window
+        self.grab_set()  # hijack all commands from the master (clicks on the main window are ignored)
+        master.wait_window(
+            self
+        )  # pause anything on the main window until this one closes
+
+    def _create_error_message_panel(self):
+        message_panel = ttk.Frame(master=self)
+        message = ttk.Label(
+            master=message_panel,
+            text="No MPRIS enabled media players are currently running!",
+        )
+        ok_button = ttk.Button(
+            master=message_panel, text="OK", command=self._handle_ok_command
+        )
+        message.grid(row=0, column=0, padx=5, pady=5)
+        ok_button.grid(row=1, column=0, padx=10, pady=5)
+        message_panel.grid()
+
+    def _create_players_selection_panel(self, player_names: List):
+        players_panel = ttk.LabelFrame(master=self, text="Players")
+        lb_height = 5
+        self._players_listbox = tk.Listbox(
+            master=players_panel,
+            listvariable=tk.StringVar(value=player_names),
+            width=20,
+            height=lb_height,
+        )
+        self._players_listbox.grid(column=0, row=0, sticky="NWES")
+        sv = ttk.Scrollbar(
+            players_panel, orient=tk.VERTICAL, command=self._players_listbox.yview
+        )
+        sv.grid(column=1, row=0, sticky="NS")
+        self._players_listbox["yscrollcommand"] = sv.set
+        sh = ttk.Scrollbar(
+            players_panel, orient=tk.HORIZONTAL, command=self._players_listbox.xview
+        )
+        sh.grid(column=0, row=1, sticky="EW")
+        self._players_listbox["xscrollcommand"] = sh.set
+        players_panel.grid_columnconfigure(0, weight=1)
+        players_panel.grid_rowconfigure(0, weight=1)
+        players_panel.grid(padx=0, pady=5)
+
+        # button_panel = ttk.LabelFrame(master=self)
+        button_panel = tk.Frame(master=self)
+        connect_button = ttk.Button(
+            master=button_panel, text="Connect", command=self._handle_connect_command
+        )
+        cancel_button = ttk.Button(
+            master=button_panel, text="Cancel", command=self._handle_cancel_command
+        )
+        connect_button.grid(row=0, column=1, padx=10)
+        cancel_button.grid(row=0, column=2, padx=10)
+        button_panel.grid_columnconfigure(0, weight=1)
+        button_panel.grid_rowconfigure(0, weight=1)
+        button_panel.grid(pady=10)
+
+    def _handle_connect_command(self):
+        player_name = self._players_listbox.get(tk.ACTIVE)
+        fq_player_name = self._running_players[player_name]
+        new_player = None
+        if fq_player_name:
+            new_player = PlayerFactory.get_player(fq_player_name, player_name)
+            ChaptersGuiBuilder.current_player.set_player(new_player)
+        self.destroy()
+
+    def _handle_cancel_command(self):
+        self.destroy()
+
+    def _handle_ok_command(self):
+        self.destroy()
+
+
+def handle_connection_command():
+    running_player_names = PlayerFactory.get_running_player_names()
+    popup = PlayerConnectionPopup(
+        master=ChaptersGuiBuilder.main_window,
+        running_players=running_player_names,
+    )
+
 
 class ChaptersGuiBuilder:
-    def __init__(self, chapters_filename: str, player: Player):
+    main_window: tk.Tk = None
+    current_player: PlayerProxy = None
+
+    def __init__(self, chapters_filename: str, player: PlayerProxy):
         self._player_control_panel: PlayerControlPanel = None
         self._chapters_panel: ChaptersPanel = None
         self._player = player
@@ -107,10 +208,26 @@ class ChaptersGuiBuilder:
             logger.error(fe)
             raise fe
         self._main_window = ChaptersGui(self._chapters_title)
+        ChaptersGuiBuilder.main_window = self._main_window
+        ChaptersGuiBuilder.current_player = self._player
 
     @property
     def chapters_gui_window(self) -> ChaptersGui:
         return self._main_window
+
+    @property
+    def player(self) -> PlayerProxy:
+        return self._player
+
+    def build_menu_bar(self):
+        menu_bar = tk.Menu(master=self._main_window)
+        self._main_window.config(menu=menu_bar)
+
+        connection_menu = tk.Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Connect", menu=connection_menu)
+        connection_menu.add_command(
+            label="Connect to ...", command=handle_connection_command
+        )
 
     def build_chapters_panel(self):
         listbox_items: List[str] = []
@@ -151,8 +268,9 @@ class ChaptersGuiBuilder:
         )
 
 
-def build_gui_menu(chapters_filename: str, player: Player) -> ChaptersGui:
+def build_gui_menu(chapters_filename: str, player: PlayerProxy) -> ChaptersGui:
     gui_builder = ChaptersGuiBuilder(chapters_filename, player)
+    gui_builder.build_menu_bar()
     gui_builder.build_chapters_panel()
     gui_builder.build_player_control_panel()
     return gui_builder.chapters_gui_window
